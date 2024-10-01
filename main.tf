d504781af16ca7c678f66d19e7e0b0920377da1a
paulocmeirelles <paulochavesmeirelles@gmail.com>
2024-10-01 08:29:13 -0300

    format terraform

--- main.tf
+++ main.tf
@@ -29,19 +29,19 @@ resource "aws_security_group" "postgresql_sg" {
   }
 
   ingress {
-    description = "Allow all TCP IPv6"
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    description      = "Allow all TCP IPv6"
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
     ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
